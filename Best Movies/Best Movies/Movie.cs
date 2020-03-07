eef7e056a6a98da99a26507a38dd8db46f33a869
Ewelina Pawłowska <e.pawlowska.97@gmail.com>
2020-03-07 11:31:58 +0100

    Movies with user rating added

---
+++ Best Movies/Best Movies/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Best_Movies
+{
+    public class Movie
+    {
+        public Movie(string title, int userRating)
+        {
+            Title = title;
+            UserRating = userRating;
+        }
+
+        public string Title { get; set; }
+        public int UserRating { get; set; }
+
+    }
+}
---
+++ Best Movies/Best Movies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+using System.Collections.Generic;
+
+
+namespace Best_Movies
+{
+    class Program
+    {
+        static void Main()
+        {
+            var listOfMovies = new List<Movie>()
+            {
+                new Movie("Saw", 7),
+                new Movie("Parasite", 9),
+                new Movie("Kler", 7),
+                new Movie("Joker", 8),
+                new Movie("Green Book", 9),
+                new Movie("1917", 6),
+                new Movie("Boże Ciało", 3),
+                new Movie("Dunkirk", 5),
+                new Movie("The invisivle man", 7),
+                new Movie("The Lego movie", 10),
+                new Movie("Lego Batman", 10),
+                new Movie("Ford vs Ferrari", 8)
+            };
+        }
+    }
+}
